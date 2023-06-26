dd855c992600bd0e4ddb8dad47840bf61cacbcd3
kajela <titoyut56@gmail.com>
2023-06-26 20:51:29 +0300

    add the initial files

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+import MoviesList from "./MoviesList,js";
+// import MoviesList from "./MoviesList";
 
 function MoviesPage({ movies }) {
   return (
