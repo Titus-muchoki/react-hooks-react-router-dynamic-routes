7fd3b5430293456ba86d29ee313e5c5569d71e76
kajela <titoyut56@gmail.com>
2023-06-26 21:01:33 +0300

    add the initial files

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList,js";
-// import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
+import { useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+  console.log(match);
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow /> 
+      </Route>
     </div>
   );
 }
